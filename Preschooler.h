ee446f5d28064596fe13919532f5c799912e4212
MontserratCL <a01713040@tec.mx>
2025-05-15 18:51:04 -0600

    Update Preschooler.h

--- Preschooler.h
+++ Preschooler.h
@@ -1,1 +1,19 @@
+#ifndef PRESCHOOLER_H
+#define PRESCHOOLER_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Preschooler : public Child {
+private: 
+    int autonomyLevel;
+public:
+    Preschooler(string name, int age, int autonomyLevel);
+    string showneeds();
+    int getAge();
+    int getAutonomyLevel(); 
+
+    };
+    #endif

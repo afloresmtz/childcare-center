b606dcb67355841128e2bb945e56d00a6ca4c3f4
Diego <181926373+Perea094@users.noreply.github.com>
2025-05-15 18:46:37 -0600

    Update Child.cpp

--- Child.cpp
+++ Child.cpp
@@ -1,1 +1,15 @@
+#include "Child.h"
 
+// Constructor default
+Child::Child() : name("No name"), age(0) {}
+
+// Constructor with parameters
+Child::Child(const std::string& name, int age) : name(name), age(age) {}
+
+// Getters
+std::string Child::getName() const {return name;}
+int Child::getAge() const {return age;}
+
+// Setters
+void Child::setName(const std::string& newName) {name = newName;}
+void Child::setAge(int newAge) {age = newAge;}

d9181d109afa06f49a14e8a3c0f1dee6e32fe00b
LuisHorca <a01713551@tec.mx>
2025-05-15 18:44:17 -0600

    Update Infant.h

--- Infant.h
+++ Infant.h
@@ -1,1 +1,18 @@
+#ifndef INFANT_H
+#define INFANT_H
+#include <iostream>
+#include <string>
+#include "Child.h"
+using namespace std;
 
+class Infant : public Child {
+private:
+    int bottles;
+public:
+    Infant(string name, int age, int bottles);
+    string showneeds();
+    int getbottles();
+    void setbottles(int amount);
+
+};
+#endif

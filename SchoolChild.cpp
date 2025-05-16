a63b7909e9dc04593b88cec34336083f4ce713ce
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-15 18:52:07 -0600

    SchoolChild.cpp

--- SchoolChild.cpp
+++ SchoolChild.cpp
@@ -1,1 +1,8 @@
+#include "SchoolChild.h"
 
+SchoolChild::SchoolChild(std:string name, int age, int level)
+: Child(name, age), readingLevel(level){}
+
+std::string SchoolChild::showNeeds(){
+    return name + "Schoolchild"
+}

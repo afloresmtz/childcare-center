5bc4ee812b0d7bea14291375f8fdd966c3e3b993
Diego <181926373+Perea094@users.noreply.github.com>
2025-05-15 18:46:20 -0600

    Update Child.h

--- Child.h
+++ Child.h
@@ -1,1 +1,29 @@
+#ifndef CHILD_H
+#define CHILD_H
 
+#include <string>
+
+class Child
+{
+private:
+    /* data */
+protected:
+    std::string name;
+    int age;
+
+public:
+    // Constructor
+    Child();
+    Child(const std::string& name, int age);
+
+    //Getters
+    std::string getName() const;
+    int getAge() const;
+
+    //Setters
+    void setName(const std::string& name);
+    void setAge(int age);
+    
+};
+
+#endif

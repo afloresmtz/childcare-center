1600d365a83fda9aac65ae42fc7b5d9e694a4250
afloresmtz <a01708363@tec.mx>
2025-05-15 18:45:20 -0600

    Added First Draft of Caregiver.h

--- Caregiver.h
+++ Caregiver.h
@@ -1,1 +1,23 @@
+#ifndef CAREGIVER_H
+#define CAREGIVER_H
 
+#include <string>
+#include <list>
+#include "Infant.h"
+#include "Preschooler.h"
+#include "SchoolChild.h"
+
+class Caregiver {
+private:
+    string name;
+    int yearsOfExp;
+    list<Infant> infantList;
+    list<Preschooler> preSchoolerList;
+    list<SchoolChild> schoolChildList;
+public:
+    Caregiver(string name, int years, list<Infant> infantList, list<Preschooler> preSchoolerList, list<SchoolChild> schoolChildList);
+
+    string displayReport();
+};
+
+#endif

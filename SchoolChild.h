715c84eed7a7ee5939fd28b75bd4414d9fc712da
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-15 18:51:46 -0600

    SchoolChild.h

--- SchoolChild.h
+++ SchoolChild.h
@@ -1,1 +1,18 @@
+#indef SCHOOLCHILD_H
+#define SCHOOLCHILD_H
 
+#include "Child.h"
+
+class SchoolChild:public Child{
+    private:
+    int readingLevel;
+
+    public:
+    SchoolChild(std::string name, int age, int level);
+    std::string showNeeds() override;
+    int getLevel();
+    void setLevel(int level);
+
+};
+
+#endif

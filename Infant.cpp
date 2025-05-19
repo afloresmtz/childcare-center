e5aa6125f26822619fd36f5ba96f9679fb07e72a
LuisHorca <a01713551@tec.mx>
2025-05-19 17:55:38 -0600

    Update Infant.cpp

--- Infant.cpp
+++ Infant.cpp
@@ -1,1 +1,18 @@
+#include "Infant.h"
+
+Infant::Infant(string name, int age, int bottles)
+    : Child(name, age), bottles(bottles) { }
+
+string Infant::showNeeds() {
+   
+    return getName() + " needs " + to_string(bottles) + " bottles.";
+}
+
+int Infant::getBottles() {
+    return bottles;
+}
+
+void Infant::setBottles(int amount) {
+    bottles = amount;
+}
 

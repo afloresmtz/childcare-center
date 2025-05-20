3753a0af6c8782378abdabe0404e414e5184b76a
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-19 18:11:28 -0600

    SchoolChild.cpp

--- SchoolChild.cpp
+++ SchoolChild.cpp
@@ -1,16 +1,21 @@
+// Incluye el header
 #include "SchoolChild.h"
 
+// Constructor. Al usar ":" nos ahorramos el especificar el atributo
 SchoolChild::SchoolChild(std:string name, int age, int level)
 : Child(name, age), readingLevel(level){}
 
+// Método de showNeeds
 std::string SchoolChild::showNeeds(){
     return name + "(Schoolchild) needs educational activities and free play.";
 }
 
+// Getter
 int SchoolChild::getLevel(){
     return readingLevel;
 }
 
+// Setter
 void SchoolChild::setLevel(int level){
     readingLevel = level;
 }

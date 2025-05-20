cce14ea5d045b9b20db3747e0bfcb33f22827f28
MontserratCL <a01713040@tec.mx>
2025-05-19 18:07:05 -0600

    Update Preschooler.cpp

--- Preschooler.cpp
+++ Preschooler.cpp
@@ -1,17 +1,14 @@
 #include "Preschooler.h"
+
 Preschooler::Preschooler(string name, int age, int autonomy) : Child(name, age) {
     autonomyLevel = autonomy;
 }
 
 string Preschooler::showNeeds() {
-    return getName() + " needs help with complex tasks but can handle basic self-care with autonomy level " + 
-           to_string(autonomyLevel);
+    return getName() + " is a preschooler with autonomy level " + to_string(autonomyLevel) + 
+           " and needs guidance with complex tasks.";
 }
 
 int Preschooler::getAuto() {
     return autonomyLevel;
-}
-
-void Preschooler::setAuto(int level) {
-    autonomyLevel = level;
 }

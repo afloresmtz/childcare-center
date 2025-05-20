8e41d7a8fa78414939ba6e51c62ce2a70aba6956
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-19 18:08:29 -0600

    SchoolChild.cpp

--- SchoolChild.cpp
+++ SchoolChild.cpp
@@ -4,5 +4,13 @@ SchoolChild::SchoolChild(std:string name, int age, int level)
 : Child(name, age), readingLevel(level){}
 
 std::string SchoolChild::showNeeds(){
-    return name + "Schoolchild"
+    return name + "(Schoolchild) needs educational activities and free play.";
+}
+
+int SchoolChild::getLevel(){
+    return readingLevel;
+}
+
+void SchoolChild::setLevel(int level){
+    readingLevel = level;
 }

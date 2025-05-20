9b09f4add9107c380d40f655bcc2d3670dec71f0
LuisHorca <a01713551@tec.mx>
2025-05-19 18:08:21 -0600

    Update Infant.h

--- Infant.h
+++ Infant.h
@@ -1,18 +1,18 @@
 #ifndef INFANT_H
 #define INFANT_H
-#include <iostream>
+
 #include <string>
 #include "Child.h"
-using namespace std;
 
 class Infant : public Child {
 private:
     int bottles;
-public:
-    Infant(string name, int age, int bottles);
-    string showNeeds();
-    int getBottles();
-    void setBottles(int amount);
 
+public:
+    Infant(const std::string& name, int age, int bottles);
+    std::string showNeeds() override;
+    int getBottles() const;
+    void setBottles(int amount);
 };
+
 #endif

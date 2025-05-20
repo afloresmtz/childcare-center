49c90c33c56bf28716713bf9ad1de6f4c94204ea
iRodama05 <iker.rdz.amaro@gmail.com>
2025-05-19 18:13:45 -0600

    SchoolChild.h

--- SchoolChild.h
+++ SchoolChild.h
@@ -1,18 +1,18 @@
 #indef SCHOOLCHILD_H
 #define SCHOOLCHILD_H
 
+// Incluye el header "Child"
 #include "Child.h"
 
 class SchoolChild:public Child{
     private:
-    int readingLevel;
+    int readingLevel; // Atributo privado de readingLevel exlusivo de SchoolChild.
 
     public:
     SchoolChild(std::string name, int age, int level);
-    std::string showNeeds() override;
-    int getLevel();
-    void setLevel(int level);
-
+    std::string showNeeds() override; // Aplica "Override" (no sé cómo sería el término) al método establecido en la superclase.
+    int getLevel(); // Getter
+    void setLevel(int level); // Setter
 };
 
 #endif

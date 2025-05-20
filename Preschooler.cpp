59f523f3fd79be352a283327ced759dae09b9199
MontserratCL <a01713040@tec.mx>
2025-05-19 18:14:58 -0600

    Update Preschooler.cpp

--- Preschooler.cpp
+++ Preschooler.cpp
@@ -1,14 +1,22 @@
 #include "Preschooler.h"
 
+// Constructor
 Preschooler::Preschooler(string name, int age, int autonomy) : Child(name, age) {
     autonomyLevel = autonomy;
 }
 
+// Override the showNeeds method from Child class
 string Preschooler::showNeeds() {
     return getName() + " is a preschooler with autonomy level " + to_string(autonomyLevel) + 
            " and needs guidance with complex tasks.";
 }
 
+// Getter
 int Preschooler::getAuto() {
     return autonomyLevel;
+}
+
+// Setter
+void Preschooler::setAuto(int level) {
+    autonomyLevel = level;
 }
